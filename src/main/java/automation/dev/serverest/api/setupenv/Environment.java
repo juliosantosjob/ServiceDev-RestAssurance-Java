6c624fa2768bb7088e489dcb9984130018f4a7e2
juliosantosjob <julio958214@gmail.com>
2024-09-23 09:55:42 -0300

    Atuando na correção da enviroments base url

--- src/main/java/automation/dev/serverest/api/setupenv/Environment.java
+++ src/main/java/automation/dev/serverest/api/setupenv/Environment.java
@@ -1,18 +1,14 @@
 package automation.dev.serverest.api.setupenv;
 
 public class Environment {
-    private static String environment = Config.get("app.environment");
-    private static String baseUrl = "";
+    private static String baseUrl;
 
     public static String getBaseUrl() {
-        if ("dev".equalsIgnoreCase(environment)) {
-            baseUrl = Config.get("app.base.url.dev");
-        } else if ("hom".equalsIgnoreCase(environment)) {
-            baseUrl = Config.get("app.base.url.hom");
-        } else if (baseUrl == null){
+
+        if (Config.get("app.base.url.hom") == null) {
             baseUrl = System.getenv("APP_BASE_URL");
         } else {
-            throw new IllegalArgumentException("Base URL is not set in config or environment variables");
+            baseUrl = Config.get("app.base.url.hom");
         }
         return baseUrl;
     }
