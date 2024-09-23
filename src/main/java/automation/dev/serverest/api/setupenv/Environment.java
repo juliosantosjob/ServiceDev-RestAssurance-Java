84454e56edc3e960ea8ad773204c30d7e1b5cc17
juliosantosjob <julio958214@gmail.com>
2024-09-23 14:38:56 -0300

    Atuando na correção da enviroments base url

--- src/main/java/automation/dev/serverest/api/setupenv/Environment.java
+++ src/main/java/automation/dev/serverest/api/setupenv/Environment.java
@@ -1,17 +1,18 @@
 package automation.dev.serverest.api.setupenv;
 
+import static java.lang.System.getProperty;
+
 public class Environment {
     private static String environment = Config.get("app.environment");
     private static String baseUrl;
-    private static String env;
-
+    private static String setEnv = getProperty("gotit", "hom-act");
 
     public static String getBaseUrl() {
-        if (env.equals("dev")) {
+        if (setEnv.equals("dev")) {
             baseUrl = Config.get("app.base.url.dev");
-        } else if (env.equals("hom")) {
+        } else if (setEnv.equals("hom")) {
             baseUrl = Config.get("app.base.url.hom");
-        } else if (env.equals("hom-act")){
+        } else if (setEnv.equals("hom-act")){
             baseUrl = System.getenv("HOM_BASE_URL");
         } else {
             throw new IllegalArgumentException("Base URL is not set in config or environment variables");
