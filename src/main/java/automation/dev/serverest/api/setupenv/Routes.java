9f6d9cfae4b23d1e2838045a4bc0692000ed81ec
juliosantosjob <juliosantosjob@users.noreply.github.com>
2024-09-24 12:09:11 +0000

    Automated deployment: Tue Sep 24 12:09:11 UTC 2024 c69f6ccf233ecd059f76e5b6579d05f93f1109c2

--- src/main/java/automation/dev/serverest/api/setupenv/Routes.java
+++
@@ -1,7 +0,0 @@
-package automation.dev.serverest.api.setupenv;
-
-public interface Routes {
-    String APP_BASE_URL = Environment.getBaseUrl();
-    String USERS = "/usuarios/";
-    String LOGIN = "/login/";
-}
--- src/test/java/automation/dev/serverest/api/base/Constants.java
+++
@@ -1,6 +0,0 @@
-package automation.dev.serverest.api.base;
-
-public interface Constants {
-    String CONTENT_TYPE = "application/json";
-    Long MAX_TIMEOUT = 3000L;
-}
